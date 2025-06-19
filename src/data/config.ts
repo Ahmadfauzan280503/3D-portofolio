132fee0d3de4c4601f409aff4acd125a66affb66
Ahmadfauzan280503 <ahmadfauzan280503@gmail.com>
2025-06-20 01:02:37 +0700

    Initial commit

---
+++ src/data/config.ts
@@ -0,0 +1,48 @@
+const config = {
+  title: "Ahmad Fauzan | Full Stack Dev",
+  description: {
+    long: "Explore the portfolio of Ahmad Fauzan, a Computer Science Engineer with 1.5 year experience in Devlopment, DevOps & Database management with hands-on experience in deploying & automating workflows, & collaborating with foreign clients. Strong analytical skills, effective communication, and a team-oriented approach to driving process improvements and delivering results.",
+    short:
+      "Discover the portfolio of Ahmad Fauzan, a DevOps enthusiast solving real life problems in DevOps and Development.",
+  },
+  keywords: [
+    "Fauzan",
+    "Ahmad Fauzan",
+    "AhmadFauzan",
+    "AhmadFauzan.com",
+    "Ahmad Fauzan portfolio",
+    "portfolio",
+    "DevOps",
+    "Devlopment",
+    "Database",
+    "AI Docker file optimizer",
+    "Finance me",
+    "creative technologist",
+    "web development",
+    "interactive websites",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    "AWS",
+    "DevSecOps",
+    "AIOps",
+    "Azure",
+  ],
+  author: "Ahmad Fauzan",
+  email: "ahmadfauzan280503@gmail.com",
+  site: "https://ahmadfauzan.com",
+
+  get ogImg() {
+    return this.site + "/assets/seo/landing-page.png";
+  },
+  social: {
+    twitter: "https://x.com/a_fauzan0305/",
+    linkedin: "https://www.linkedin.com/in/ahmad-fauzan-f28a05u03/",
+    instagram: "https://www.instagram.com/a.fauzan03/",
+    facebook: "https://www.facebook.com/profile.php?id=100010029821759",
+    github: "https://github.com/Ahmadfauzan280503/",
+  },
+};
+export { config };
